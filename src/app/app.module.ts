8f7175bfb60ca72dbf85d8a51cf41a5749957056
Sarthak Srivastava <sarthak321321@gmail.com>
2020-08-23 02:26:54 +0530

    First Draft

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MatTableModule } from '@angular/material/table';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatPaginatorModule, DateAdapter, MatNativeDateModule } from '@angular/material';
+import { MatFormFieldModule, MatInputModule, MatSortModule, MatToolbarModule, MatIconModule,
+MatDatepickerModule } from '@angular/material';
+import { DatePipe } from '@angular/common';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
+    MatIconModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSortModule,
+    MatPaginatorModule,
+    MatTableModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [DatePipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,96 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {MatTableDataSource} from '@angular/material/table';
+import {ViewChild} from '@angular/core';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
+import { DatePipe } from '@angular/common';
+
+export class UserData {
+  orderid: number;
+  Customerid: number;
+  deliveryPincode: string;
+  orderDate: string;
+  items: string;
+  constructor(
+    orderid: number,
+    Customerid: number,
+    deliveryPincode: string,
+    orderDate: string,
+    items: string
+  ) {
+    this.orderid = orderid;
+    this.Customerid = Customerid;
+    this.deliveryPincode = deliveryPincode;
+    this.orderDate = orderDate;
+    this.items = items;
+  }
+}
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+
+export class DashboardComponent implements OnInit {
+  userArray: MatTableDataSource<UserData>;
+
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
+
+  constructor(
+    private http: HttpClient,public datepipe: DatePipe) {
+      this.http.get('assets/SeedData.csv', {responseType: 'text'}).subscribe(
+        data => {
+          const csvToRowArray = data.split('\n');
+          const t = new Array(csvToRowArray.length - 1);
+          for (let index = 1; index <= csvToRowArray.length - 1; index++) {
+            const row = csvToRowArray[index].split(',');
+            const timestamp = Date.parse(row[3]);
+            let k = '';
+            if (isNaN(timestamp) === false) {
+            const m = new Date(row[3]);
+            k = this.datepipe.transform(m, 'dd/MM/yyyy');
+            }
+            t[index - 1] = new UserData(
+              parseInt(row[0], 10),
+              parseInt(row[1], 10),
+              row[2],
+              k,
+              row[4]
+            );
+          }
+          this.userArray = new MatTableDataSource(t);
+          this.userArray.paginator = this.paginator;
+          this.userArray.sort = this.sort;
+        },
+        error => {
+          console.log(error);
+      }
+      );
+      // this.userArray = new MatTableDataSource();
+     }
+  displayedColumns: string[] = ['orderid', 'Customerid', 'deliveryPincode', 'orderDate', 'items'];
+
+  ngOnInit(): void {
+  }
+  applyFilterPincode(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.userArray.filterPredicate = (data, filter) =>
+      (data.deliveryPincode.indexOf(filter) != -1);
+    this.userArray.filter = filterValue.trim().toLowerCase();
+    if (this.userArray.paginator) {
+      this.userArray.paginator.firstPage();
+    }
+  }
+  applyFilterDate(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.userArray.filterPredicate = (data, filter) =>
+      (data.orderDate.indexOf(filter) != -1);
+    this.userArray.filter = filterValue.trim().toLowerCase();
+    if (this.userArray.paginator) {
+      this.userArray.paginator.firstPage();
+    }
+  }
+}
