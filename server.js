fe474a8163ccf4a31fb8ea5ae62587cb2f73877a
Sarthak Srivastava <sarthak321321@gmail.com>
2020-08-23 12:29:47 +0530

    test heroku deployment

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/antstackdashboard'));
+app.use(express.static('./dist/antstackdashboard'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/antstackdashboard/index.html'));
-});
+app.get('/*', function(req, res){
+    res.sendFile('index.html', {root: 'dist/antstackdashboard/'}
+  );
+  });
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
